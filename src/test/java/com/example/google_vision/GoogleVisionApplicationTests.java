a56e18a3b573a972218c6bed1a76cf8082c43e7a
unknown <fahmiramdhani.st@gmail.com>
2025-07-04 09:13:27 +0700

    first setup repository

---
+++ src/test/java/com/example/google_vision/GoogleVisionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.google_vision;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoogleVisionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
